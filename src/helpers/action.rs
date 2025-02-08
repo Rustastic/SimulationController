892b72a571c79d9474c70ad611a66798a5acd649
ilBuso <alessandro.busola@gmail.com>
2025-02-08 23:44:22 +0100

    debug

--- src/helpers/action.rs
+++ src/helpers/action.rs
@@ -164,37 +164,27 @@ pub fn remove_sender(
         Ok(neighbor) => match verify::is_a_neighbor(neighbor, to_remove, node_id, false) {
             Ok(()) => {
                 if sim_ctrl.drones.contains_key(to_remove) {
-                    println!("\n1\n");
-
                     sim_ctrl.handle_drone_command(&to_remove, DroneCommand::RemoveSender(*node_id));
                     Ok(())
                 } else if sim_ctrl.cclients.contains_key(to_remove) {
-                    println!("\n2\n");
-
                     sim_ctrl.handle_cclient_command(
                         &to_remove,
                         ChatClientCommand::RemoveSender(*node_id),
                     );
                     Ok(())
                 } else if sim_ctrl.mclients.contains_key(to_remove) {
-                    println!("\n3\n");
-
                     sim_ctrl.handle_mclient_command(
                         &to_remove,
                         MediaClientCommand::RemoveSender(*node_id),
                     );
                     Ok(())
                 } else if sim_ctrl.comm_servers.contains_key(to_remove) {
-                    println!("\n4\n");
-
                     sim_ctrl.handle_commserver_command(
                         &to_remove,
                         CommunicationServerCommand::RemoveSender(*node_id),
                     );
                     Ok(())
                 } else {
-                    println!("\n5\n");
-
                     sim_ctrl
                         .handle_drone_command(&to_remove, DroneCommand::RemoveSender(*node_id));
                     Ok(())
