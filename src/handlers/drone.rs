d0e56ae78c725cd923bdf4b7f144d73c1957c713
ilBuso <alessandro.busola@gmail.com>
2025-06-14 15:21:56 +0200

    debug

--- src/handlers/drone.rs
+++ src/handlers/drone.rs
@@ -16,11 +16,11 @@ impl SimulationController {
     pub fn handle_drone_event(&self, drone_event: DroneEvent) {
         match drone_event {
             DroneEvent::PacketSent(packet) => {
-                info!(
+                /*info!(
                     "[ {} ] Is a {}",
                     "Simulation Controller".yellow(),
                     packet.pack_type
-                );
+                );*/
 
                 if let Some(src) = packet
                     .routing_header
