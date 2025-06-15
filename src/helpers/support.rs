d6fde0f2af4e91da7c455c8a38b6a37c81c50d79
ilBuso <alessandro.busola@gmail.com>
2025-06-15 21:10:35 +0200

    debug

--- src/helpers/support.rs
+++ src/helpers/support.rs
@@ -160,9 +160,6 @@ impl SimulationController {
 
     pub fn remove_sender(&mut self, node_id: NodeId, to_remove: NodeId) -> Result<(), Error> {
         // Check if it exists a neighbor with this id
-
-        error!("SIM - 4");
-
         match self.has_neighbors(node_id) {
             Ok(neighbor) => { 
                 match super::verify::is_a_neighbor(neighbor, to_remove, node_id, false) {
