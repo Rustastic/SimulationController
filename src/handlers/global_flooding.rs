02e54c3521a8a495c5dd93155e01715c7732ba06
ilBuso <alessandro.busola@gmail.com>
2025-06-18 16:27:56 +0200

    debug

--- src/handlers/global_flooding.rs
+++ src/handlers/global_flooding.rs
@@ -8,7 +8,7 @@ use crate::SimulationController;
 impl SimulationController {
     pub fn global_flooding(&mut self) {
         /* REMOVE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
-        std::thread::sleep(std::time::Duration::from_secs(2));
+        // std::thread::sleep(std::time::Duration::from_secs(2));
 
         for (chat_client, (_, _)) in self.cclients.clone() {
             self.handle_chat_client_command(&chat_client, ChatClientCommand::InitFlooding);
